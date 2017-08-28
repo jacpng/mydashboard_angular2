fc1afe1d1d1f2ff5b6ed9efb615bf50a9acba767
Theng Hao Png <jacpng@gmail.com>
2017-08-28 22:21:05 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
+import { HttpService} from './http.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+coaches
+	constructor(public httpService : HttpService){
+
+		this.httpService.getCoaches()
+		.subscribe(data => {
+			console.log(data)
+			this.coaches = data 
+		})
+
+	}
+
   title = 'app';
+  augustBaby ='Franklin';
+  newcoach = {
+  	name: '',
+  	description:'',
+  	image_url:''
+  }
+
+	saveCoach(newcoach){
+		this.httpService.addCoach(newcoach)
+		.subscribe(data => {
+			console.log(data)
+
+		this.coaches.push(newcoach)
+  		this.newcoach ={
+  			name:'',
+  			description:'',
+  			image_url:''
+
+  		}
+
+  	})
+  	}
 }
+
+
+
+
+    /* [{
+  	name:'Yunkie',
+  	description:'special in golf',
+  	image_url:'http:www.facebook.com'
+
+
+  },
+  {
+  	name:'Franklin',
+  	description:'special in weight loss',
+  	image_url:'http:www.facebook.com'
+
+  },
+  {
+  	name:'Ken',
+  	description:'weight lifting',
+  	image_url:'http:www.facebook.com'
+  }
+
+  	]*/
+
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HttpService } from './http.service';
+import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
